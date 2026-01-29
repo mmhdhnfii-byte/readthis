29073dc1968eceaae90b07e841aeb1a83d7273d4
mmhdhnfii-byte <mmhdhnfii@gmail.com>
2026-01-29 09:44:02 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-const message =Sejak kau ada di sini, dunia yang dulu biasa saja pelan-pelan belajar jadi hangat.
-Tawa kecilmu terasa seperti lagu yang terus terulang di kepalaku, sederhana tapi menenangkan.
-Aku tak minta banyak—cukup tetap di sini,
-karena nyatanya, hadirku lebih utuh saat kau ada.
+const message = `I don’t even know if you’ll ever read this.\nBut if you do… I just want you to know, I still think about you sometimes.\n\nWe both tried. We both cared. But the universe just never gave us enough space to become “us.”\n\nI just wonder… if we had met at a different point in life, would the ending have been different?\n\nAnyway, I hope you're doing okay, even though we’re not in each other’s lives anymore.`;
 
 function showLetter() {
   const audio = document.getElementById("bgm");
@@ -28,4 +25,3 @@ function showLetter() {
     typeWriter();
   }, 600);
 }
-
