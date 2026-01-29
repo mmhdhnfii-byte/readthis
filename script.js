e11e995eb03a47c885ca106300f2acaf6b8e2192
mmhdhnfii-byte <mmhdhnfii@gmail.com>
2026-01-29 09:35:10 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-const message = `I don’t even know if you’ll ever read this.\nBut if you do… I just want you to know, I still think about you sometimes.\n\nWe both tried. We both cared. But the universe just never gave us enough space to become “us.”\n\nI just wonder… if we had met at a different point in life, would the ending have been different?\n\nAnyway, I hope you're doing okay, even though we’re not in each other’s lives anymore.`;
-
 function showLetter() {
+  const audio = document.getElementById("bgm");
+  audio.volume = 0.5;
+  audio.play(); 
+
   document.getElementById("introText").style.opacity = 0;
   document.querySelector(".btn").style.display = "none";
 
@@ -21,6 +23,3 @@ function showLetter() {
     typeWriter();
   }, 600);
 }
-
-
-
